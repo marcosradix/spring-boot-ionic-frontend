a1d742b9e5e139fd183d68b875b92cc0462df27e
Marcos Ferreira <marcosradix@gmail.com>
2018-12-01 10:27:10 -0300

    tela inicial criada

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { IonicPage } from 'ionic-angular/navigation/ionic-page';
+import { Slides } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,9 +9,22 @@ import { IonicPage } from 'ionic-angular/navigation/ionic-page';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild(Slides) slides: Slides;
   constructor(public navCtrl: NavController) {
 
+  }
+  src:string=`/assets/imgs/slide1.jpeg`;
+
+  goToSlide() {
+    this.slides.slideTo(1, 500);
+  }
+  slideChanged() {
+    let currentIndex = this.slides.getActiveIndex();
+    if(this.slides.isEnd){
+      this.src = `/assets/imgs/slide${1}.jpeg`;
+    }
+    this.src = `/assets/imgs/slide${currentIndex+1}.jpeg`;
+    console.log('Current index is', currentIndex);
   }
 
 }
