4d6831578ba5f0bbfd6fa5f7be33d5424a3c8388
Marcos Ferreira <marcosradix@gmail.com>
2018-12-17 20:38:55 -0300

    criada página de detalhe do produto

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -3,6 +3,9 @@ import { ProdutoService } from './../../service/domain/produto.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../model/produto.dto';
+import { CategoriaService } from '../../service/domain/categoria.service';
+import { CategoriaDTO } from '../../model/categoria.dto';
+import { Observable } from 'rxjs';
 
 
 @IonicPage()
@@ -14,15 +17,27 @@ export class ProdutosPage {
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    public produtoService: ProdutoService) {
+    public produtoService: ProdutoService,
+    public categoriaService: CategoriaService) {
   }
 
   items: Array<ProdutoDTO> = [];
-
+  categoria: CategoriaDTO;
+  categorias: Observable<CategoriaDTO[]>;
+  categoriaNome: string ="";
   ionViewDidLoad() {
     let categoria_id = this.navParams.get("categoria_id");
     this.produtoService.findByCategoria(categoria_id).subscribe(response => {
+      this.categorias = this.categoriaService.findAll();
       this.items = response['content'];
+      this.categorias.forEach(c =>{
+        c.forEach(ct =>{
+          if(ct.id == categoria_id){
+            this.categoria = ct;
+            this.categoriaNome = this.categoria.nome;
+          }
+        });
+      });
       this.loadImageUrls();
     },
       error => console.log("Error ", error));
@@ -37,6 +52,10 @@ export class ProdutosPage {
         error => {});
     }
 
+  }
+
+  showDetail(){
+    this.navCtrl.push("ProdutoDetailPage");
   }
 
 }
